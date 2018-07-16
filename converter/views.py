04fa7941da1d7d419884c598c3c857b4637efcba
Lucas Abreu <lucasabreuapi@gmail.com>
2018-07-16 16:50:25 -0300

    novo layout

--- converter/views.py
+++ converter/views.py
@@ -4,20 +4,22 @@ from django import forms
 from django.core.files.storage import FileSystemStorage
 from wand.image import Image
 from django.conf import settings
+import os
 
-import ipdb; ipdb.set_trace()
+
 #DESCOBRIR ONDE O WAND SALVA AS IMAGENS
 def upload_pdf(request):
 	if request.method == 'POST':
 		file = request.FILES['myfile']
 		s1,s2 = file.name.split('.')
 		new_name=s1+'.jpg'
-		file.seek(0)
+		USER_PROFILE = os.path.expandvars("%USERPROFILE%")
+		filename_output = os.path.join(USER_PROFILE, "Desktop", "destino", new_name);
 		with Image(file=file, resolution=300) as img:
 			print("pages= ", len(img.sequence))
 			with img.convert("jpg") as converted:
 				global new_name
-				converted.save(filename=new_name)
+				converted.save(filename=filename_output)
 
 
 	return render(request, 'converter/index.html')
